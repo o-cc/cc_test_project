a3cffd8d4cad7ed4b6ab2457cfcd4fb4b5480c43
ozx <359835703@qq.com>
2019-01-30 17:54:34 +0800

    导入全局的global value

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
 import login from '@/components/login'
-import globalVal from "../globalVal/globalTips"
+import globalVal from "../globalValue"
+import hotelSearch from '@/components/hotelSearch'
 
 global.globalVal = globalVal;
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: login
+    },
+    {
+      path: '/hotelSearch',
+      name: 'hotelSearch',
+      component: hotelSearch
     }
   ]
 })

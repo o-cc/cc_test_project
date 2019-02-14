dfaac788bb5125d83b7f74a7801618f89eea52b9
ozx <359835703@qq.com>
2019-02-14 15:53:05 +0800

    修改为80端口、16进制颜色转换rgba

--- src/tool/formatDate.js
+++ src/tool/formatDate.js
@@ -72,12 +72,34 @@ formatDateObj.DateFomatTransformToChineseFomat = function ( date ) {
   }
 
 
+};
 
-}
+function colorRgb ( Hex ) {
 
+  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
 
+  var sColor = Hex.toLocaleLowerCase();
+  if( sColor && reg.test(sColor) ){
+    if( sColor.length === 4 ){
+      var sColorNew = "#";
+      for(var i=1; i<4; i+=1){
+        sColorNew += sColor.slice(i,i+1).concat(sColor.slice(i,i+1));
+      }
+      sColor = sColorNew;
+    }
+    //处理六位的颜色值
+    var sColorChange = [];
+    for(var i=1; i<7; i+=2){
+      sColorChange.push(parseInt("0x"+sColor.slice(i,i+2)));
+    }
+    return "rgba(" + sColorChange.join(",") + ",.3)";
+  }else{
+    return sColor;
+  }
+};
 
 
 export default {
-  formatDateObj: formatDateObj
+  formatDateObj: formatDateObj,
+  colorRgb: colorRgb
 }

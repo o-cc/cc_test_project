63b922b3abd1f7f0bd42af6f208938fe9f1838a3
ozx <359835703@qq.com>
2019-02-18 00:39:20 +0800

    完成部分订单数据 最新hotelInfo.js

--- src/components/childComponents/orderDetail.vue
+++ src/components/childComponents/orderDetail.vue
@@ -22,9 +22,31 @@
 
       <div class="order_date">
         <div>
-          <p>8月20号 <span style="color: #d0cfe5; font-size: .4rem; margin-left: .2rem;">周三</span></p>
-          <p><span style="padding: 0.1rem .3rem;border: 1px solid #d4d3e8;border-radius: .5rem;">8晚</span></p>
-          <p><span style="color: #d0cfe5; font-size: .4rem; margin-right: .2rem;">周日</span > 8月29号</p>
+          <!--<p>8月20号 <span style="color: #d0cfe5; font-size: .4rem; margin-left: .2rem;">周三</span></p>-->
+          <!--<p><span style="padding: 0.1rem .3rem;border: 1px solid #d4d3e8;border-radius: .5rem;">8晚</span></p>-->
+          <!--<p><span style="color: #d0cfe5; font-size: .4rem; margin-right: .2rem;">周日</span > 8月29号</p>-->
+          <div class="date_picker">
+            <group>
+              <calendar title="title" v-model="dateTimeValue" :display-format="formatDate"
+                        @on-change="dateChange" :start-date="startDate"></calendar>
+            </group>
+          </div>
+
+          <div class="date_txt">
+            <p class="first_date">
+              <span>{{ firstDateObj.date }}</span>
+              <span style="color:#bfbec6;padding-left: .1rem;font-size: 0.3rem">{{ firstDateObj.weekDay}}</span>
+
+            </p>
+            <p class="cent_p">
+              {{totalDay}}
+            </p>
+            <p class="last_date">
+              <span> {{ lastDateObj.date }}</span>
+              <span style="color:#bfbec6;padding-left: .1rem;font-size: 0.3rem">{{lastDateObj.weekDay}}</span>
+            </p>
+          </div>
+
         </div>
       </div>
 
@@ -119,27 +141,57 @@
 </template>
 
 <script>
-  import { Group,PopupPicker  } from 'vux'
+  import { Group,PopupPicker,Calendar  } from 'vux'
 
   export default {
       name: "orderDetail",
       components: {
         Group,
-        PopupPicker
-
+        PopupPicker,
+        Calendar
       },
+    mounted () {
+        this.run();
+
+    },
       data () {
         return　{
           roomNum : 1,
           personNum: 1,
 
           pickValue: [],
-          pickDataOfInvoice: ["开具发票","不开发票"]
-
+          pickDataOfInvoice: ["开具发票","不开发票"],
+          dateTimeValue  : [],
+          firstDateObj   : {
+            date   : "",
+            weekDay: "请选择日期"
+          },
+          lastDateObj    : {
+            date   : "",
+            weekDay: "请选择日期"
+          },
+          startDate      : "",
+          totalDay       : "请选择",
+          roomId         : ""
         }
 
       },
       methods: {
+        run() {
+          let self = this;
+          //获取首页选择的日期
+          let date       = new Date();
+          let month      = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+          self.startDate = date.getFullYear() + "-" + month + "-" + date.getDate();
+          //入离日期
+          let dateTimeValue  = global.globalVal.hotelInfo.hotelInfoSingleOne.hotelTempInfo.dateTimeValue;
+          self.dateTimeValue = dateTimeValue ? dateTimeValue : self.dateTimeValue;
+
+          //获取房间id
+          let roomInfo = global.globalVal.hotelInfo.hotelInfoSingleOne.hotelTempInfo.hotelRoomInfo;
+
+        },
+
         reduceRoom () {
           if( this.roomNum > 0 ) {
             this.roomNum--;
@@ -165,8 +217,37 @@
 
         addRoom () {
           this.roomNum ++;
-        }
+        },
 
+        formatDate() {
+          return this.dateTimeValue.length + "晚";
+        },
+        dateChange() {
+          let self = this;
+
+          this.dateTimeValue.sort( ( a, b ) => {
+            let val1 = new Date( a );
+            let val2 = new Date( b );
+
+            return val1 - val2;
+          } );
+          //排序好之后 第一个日期和最后一个显示出来，
+
+          let firstDate             = new Date( self.dateTimeValue[ 0 ] );
+          let firstDateWeekDate     = "周" + global.globalVal.formatDate.formatDateObj.WeekNumTransformToChineseNum( firstDate.getDay() );
+          self.firstDateObj.weekDay = firstDateWeekDate;
+          //格式化日期
+          self.firstDateObj.date    = (firstDate.getMonth() + 1) + "月" + (firstDate.getDate()) + "日";
+
+          let lastDate             = new Date( self.dateTimeValue[ self.dateTimeValue.length - 1 ] );
+          let lastDateWeekDate     = "周" + global.globalVal.formatDate.formatDateObj.WeekNumTransformToChineseNum( lastDate.getDay() );
+          self.lastDateObj.weekDay = lastDateWeekDate;
+          //格式化日期
+          self.lastDateObj.date    = (lastDate.getMonth() + 1) + "月" + (lastDate.getDate()) + "日";
+          self.totalDay            = self.formatDate();
+          global.globalVal.hotelInfo.hotelInfoSingleOne.hotelTempInfo.dateTimeValue = self.dateTimeValue;
+
+        },
       }
     }
 </script>
@@ -246,6 +327,65 @@
         justify-content: space-between;
         align-items: center;
         white-space: nowrap;
+        position: relative;
+
+        .date_picker {
+          width: 100%;
+          height: 1.5rem;
+          overflow: hidden;
+          z-index: 1;
+          position: relative;
+
+          > div {
+            opacity: 0;
+
+          }
+        }
+
+        .date_txt {
+          display: flex;
+          justify-content: space-around;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 1.5rem;
+
+          > p {
+            position: absolute;
+            top: 0%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            font-size: .2rem;
+          }
+
+          .first_date {
+            left: 0;
+            font-size: 0.4rem;
+            padding-left: .4rem;
+          }
+
+          .cent_p {
+            left: 50%;
+            top: 50%;
+            height: 50%;
+            border-radius: 1rem;
+            border: 1px solid #f2f2f2;
+            padding: 0 .3rem;
+            transform: translateX(-50%) translateY(-50%);
+          }
+
+          .last_date {
+            right: 0%;
+            font-size: 0.4rem;
+            padding-right: .3rem;
+
+          }
+        }
+
 
         >p {
           flex: 1;
@@ -291,7 +431,9 @@
 
             }
 
-
+            .add {
+              line-height: .6rem;
+            }
 
             >input {
               border: 0;
@@ -411,6 +553,22 @@
     }
 
   }
+
+  .bullet_item {
+    width: 8px;
+    height: 8px;
+    display: inline-block;
+    border-radius: 100%;
+    background: #000;
+    opacity: .4;
+    margin: 0 .1rem;
+  }
+
+  .bullet_item_active {
+    background: #f8f8f8;
+    opacity: 1;
+  }
+
 
 
 </style>
--- src/globalVal/hotelInfo/hotelInfo.js
+++ src/globalVal/hotelInfo/hotelInfo.js
@@ -1,45 +1,27 @@
 let axios = require( "axios" );
 
-let hotelInfo = undefined;
-
-function setHotelInfo ( hotelinfo ) {
-
-  hotelInfo = hotelinfo;
-};
-
-
-function getHotelInfoIncache () {
-  return hotelInfo;
-
-};
-function getHotelInfo ( city, input, callback ) {
+/*
+* params1 city [string] 选择的城市
+* params2 input [string] 搜索框内容
+* params3 [ Function ] 回调函数
+* */
+function getHotelInfo( city, input, callback ) {
 
   axios.get( global.globalVal.httpServerUrl.getHotelInfo, {
     params: {
-      city: "增城",
+      city : "增城",
       input: "万达"
     }
-  })
+  } )
     .then( res => {
       let data = res.data;
 
       callback( null, data );
       setHotelInfo( data );
-    })
+    } )
     .catch( err => {
       callback( err, null );
-    })
-
-};
-
-let hotelPageVueObj = null;
-function getHotelPageVueObj () {
-  return hotelPageVueObj;
-
-};
-
-function setHotelPageVueObj ( obj ) {
-  hotelPageVueObj = obj;
+    } )
 
 };
 
@@ -47,8 +29,8 @@ function setHotelPageVueObj ( obj ) {
 * params1 [ Number ] 酒店ID
 * params2 [ Function ] 回调函数
 * */
-function getHotelDetailInfoById ( id, callback ) {
-  axios.get( global.globalVal.httpServerUrl.getHotelInfo + id +"/" )
+function getHotelDetailInfoById( id, callback ) {
+  axios.get( global.globalVal.httpServerUrl.getHotelInfo + id + "/" )
     .then( res => {
 
       /*{
@@ -90,24 +72,118 @@ function getHotelDetailInfoById ( id, callback ) {
 
       let data = res.data;
       callback( null, data );
-    })
+    } )
     .catch( err => {
       callback( err, null );
-    })
+    } )
+
+};
+
+/*
+* params1 [ Number ] 酒店ID
+* params2 [ Function ] 回调函数
+* */
+function postAddFavorite( hotelId, callback ) {
+
+  axios.post( global.globalVal.httpServerUrl.favoriteHotel + hotelId + "/hotels/", {}, {
+    headers: {
+      "Authorization": window.localStorage.getItem( "token" ),
+    }
+  } )
+    .then( function ( res ) {
+      return callback( null, true );
+
+    } )
+    .catch( function ( err ) {
+      console.log( err );
+      return callback( "无法添加收藏", null );
+
+    } )
+
+};
+
+/*
+* params1 [ Function ] 回调函数
+* */
+function getFavoriteHotelIds( callback ) {
+
+  axios.get( global.globalVal.httpServerUrl.AllfavoriteHotelId, {
+    headers: {
+      "Authorization": window.localStorage.getItem( "token" )
+    }
+  } )
+    .then( function ( res ) {
+      //{"collected_hotels_id": [ 1, 2 ]}
+      let data = res.data;
+      return callback( null, data );
+
+    } )
+    .catch( function ( err ) {
+      console.log( err );
+      return callback( "无法获取收藏", null );
+
+    } )
+
+};
+
+/*
+* params1 [ Number ] 酒店ID
+* params2 [ Function ] 回调函数
+* */
+function postCancelFavorite( hotelId, callback ) {
+
+  axios.delete( global.globalVal.httpServerUrl.favoriteHotel + hotelId + "/hotels/", {
+    headers: {
+      "Authorization": window.localStorage.getItem( "token" ),
+    }
+  } )
+    .then( function ( res ) {
+      return callback( null, true );
+
+    } )
+    .catch( function ( err ) {
+      console.log( err );
+      return callback( "无法取消收藏", null );
+
+    } )
 
 };
 
 
+
+//=============分割线===================
+
+let hotelInfo = undefined;
+
+function setHotelInfo( hotelinfo ) {
+  hotelInfo = hotelinfo;
+};
+
+function getHotelInfoIncache() {
+  return hotelInfo;
+};
+
+let hotelPageVueObj = null;
+
+function getHotelPageVueObj() {
+  return hotelPageVueObj;
+};
+
+function setHotelPageVueObj( obj ) {
+  hotelPageVueObj = obj;
+};
+
+//hotel的临时变量 可变的 这里保存hotelId 因为酒店详细页面导入了
 const hotelTempInfo = {
-  hotelId: null
+  hotelId         : null,
+  showHomePageComp: null,
+  hotelRoomInfo   : null
 };
 
+//怎么觉得es6的写法有点丑陋？
 export default {
 
-  getHotelInfo,
-
   hotelInfoSingleOne: {
-
     getHotelInfoIncache,
     setHotelInfo,
     setHotelPageVueObj,
@@ -115,8 +191,10 @@ export default {
     hotelTempInfo
   },
 
-  getHotelDetailInfoById
-
-
+  getHotelInfo,
+  getHotelDetailInfoById,
+  postAddFavorite,
+  postCancelFavorite,
+  getFavoriteHotelIds
 
 }

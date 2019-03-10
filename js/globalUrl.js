8a46f7d01fd130c655c6f6040bc88dcd72a828ee
ozx <359835703@qq.com>
2019-03-10 17:59:46 +0800

    新增一个忘记密码

--- js/globalUrl.js
+++ js/globalUrl.js
@@ -1,8 +1,8 @@
 const baseUrl = "http://127.0.0.1:5000/api/";
 var globalUrl = {
     httpServerUrl: {
-        signIn  : baseUrl + "authorizations/",
-        register: baseUrl + "users/"
-
+        signIn              : baseUrl + "authorizations/",
+        register            : baseUrl + "users/",
+        getQuestionByUsename: baseUrl + "passwords/"
     }
 }

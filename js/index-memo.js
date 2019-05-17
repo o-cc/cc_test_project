102a0d569212a8a4f81633feac7cb206303757b8
cc <359835703@qq.com>
2019-05-18 01:36:40 +0800

    备忘录和记事本修复响应首页

--- js/index-memo.js
+++ js/index-memo.js
@@ -1,6 +1,104 @@
 function memoModlueFile(){
 
 
+    function timeEvent() {
+        var timeOutEvent = null;
+        $( ".memo_item" ).on( {
+
+            touchstart: function ( e ) {
+                // 长按事件触发
+                let self = this;
+                timeOutEvent = setTimeout( function () {
+                    timeOutEvent = 0;
+                    let memoId = $( self ).attr( "data" );
+                    if( memoId && memoId !== "undefined" ) {
+
+                        $.confirm( {
+                            title   : '提示',
+                            text    : '确定删除？',
+                            onOK    : function () {
+                                //点击确认
+                                memoModule.delteMemoInfoById( memoId, function ( err, res ) {
+                                    if( err ) {
+                                        loger( err );
+                                        return;
+                                    }
+
+                                    $( self ).remove();
+
+                                    if( $( ".memo_item" ).length < 6 ) {
+                                        let len = $( ".memo_item" ).length;
+                                        for ( let i = 0; i < 6 - len; i++ ) {
+                                            $( ".memo_items" ).append( "<div class='memo_item' style='opacity: 0'></div>" );
+                                        }
+                                    }
+                                    //删除后没有让首页盒子的消失
+                                    //写两部分，第一如果存在id和type的话，就删除 如果不存在 是不是新添加的
+                                    //$( self ).attr( "data") 是当前id
+
+                                    if( $( ".home_items>div[id="+memoId+"][data-type='memo']" ) ) {
+
+                                        try {
+                                            $( ".home_items>div[id="+memoId+"][data-type='memo']" ).remove();
+                                        }catch ( e ) {
+                                            console.log( "remove home_item fail" );
+                                            console.log( e );
+                                        }
+
+                                    }else if( $( ".home_items>div[data-id='hide'][data-type='memo']" ) ){
+                                        //如果是新增加的 是没有id的，
+                                        try {
+                                            $( ".home_items>div[data-id='hide'][data-type='memo']" ).remove();
+                                        }catch ( e ) {
+                                            console.log( "remove home_item data-id=hide fail" );
+                                            console.log( e );
+                                        }
+                                    }
+
+
+                                    if( $( ".home_item" ).length < 7 ) {
+                                        let len = $( ".home_item" ).length;
+                                        for ( let i = 0; i < 7 - len; i++ ) {
+                                            $( ".home_items" ).append( "<div class='home_item' data-type='hide' style='opacity: 0'></div>" );
+                                        }
+                                    }
+
+
+                                } )
+
+                            }
+                        } );
+                    }
+
+
+                }, 1000 );
+                //长按400毫秒
+                e.preventDefault();
+            },
+            touchmove : function () {
+                clearTimeout( timeOutEvent );
+                timeOutEvent = 0;
+            },
+            touchend  : function () {
+                clearTimeout( timeOutEvent );
+                let self = this;
+                if ( timeOutEvent != 0 ) {
+                    // 点击事件
+                    //显示到记事本
+                    if( $( self ).attr( "data" ) || $(self).attr("data-id") ) {
+                        $(".memo_title").val( $( this ).children("div").eq(0).children("h5").eq(0).html() );
+                        editor.txt.text( $( this ).children("div").eq(1).children("p").eq(0).html() );
+                        GoHashUrl( "memo_input" );
+                    }
+
+                }
+                return false;
+            }
+        } )
+
+    }
+
+
     let memoInfoIncache = memoModule.memoInfoIncache;
     memoInfoIncache     = memoInfoIncache?memoInfoIncache:" ";
     let len             = memoInfoIncache.length;
@@ -44,68 +142,7 @@ function memoModlueFile(){
     }
 
         //长按事件
-        var timeOutEvent = null;
-        $( ".memo_item" ).on( {
-
-            touchstart: function ( e ) {
-                // 长按事件触发
-                let self = this;
-                timeOutEvent = setTimeout( function () {
-                    timeOutEvent = 0;
-
-                    if( $( self ).attr( "data") ) {
-
-                        $.confirm( {
-                            title   : '提示',
-                            text    : '确定删除？',
-                            onOK    : function () {
-                                //点击确认
-                                memoModule.delteMemoInfoById( $( self ).attr( "data"), function ( err, res ) {
-                                    if( err ) {
-                                        loger( err );
-                                        return;
-                                    }
-
-                                    $( self ).remove();
-
-                                    if( $( ".memo_item" ).length < 6 ) {
-                                        let len = $( ".memo_item" ).length;
-                                        for ( let i = 0; i < 6 - len; i++ ) {
-                                            $( ".memo_items" ).append( "<div class='memo_item' style='opacity: 0'></div>" );
-                                        }
-                                    }
-                                } )
-
-                            }
-                        } );
-                    }
-
-
-                }, 1000 );
-                //长按400毫秒
-                 e.preventDefault();
-            },
-            touchmove : function () {
-                clearTimeout( timeOutEvent );
-                timeOutEvent = 0;
-            },
-            touchend  : function () {
-                clearTimeout( timeOutEvent );
-                let self = this;
-                if ( timeOutEvent != 0 ) {
-                    // 点击事件
-                    //显示到记事本
-                    if( $( self ).attr( "data" ) ) {
-                        $(".memo_title").val( $( this ).children("div").eq(0).children("h5").eq(0).html() );
-                        editor.txt.html( $( this ).children("div").eq(1).children("p").eq(0).html() );
-                        GoHashUrl( "memo_input" );
-                    }
-
-                }
-                return false;
-            }
-        } )
-
+        timeEvent();
 
     }catch ( e ) {
         console.log( e );
@@ -162,7 +199,7 @@ function memoModlueFile(){
             return;
         }
 
-        if( !editor.txt.html() ) {
+        if( !editor.txt.text() ) {
             loger( "请填写内容" );
             return;
         }
@@ -170,7 +207,7 @@ function memoModlueFile(){
         let boo = $( ".audio_click" ).prop( "checked" )?true:false;
         let changeData = {
             "title"    : $(".memo_title").val(),
-            "content"  : editor.txt.html(),
+            "content"  : editor.txt.text(),
             "is_warn"  : boo,
             "warn_time": boo?choseDate: null,
         };
@@ -182,11 +219,11 @@ function memoModlueFile(){
             .then( res => {
 
                 //将最新的数据写入到html上
-                let content = editor.txt.html();
+                let content = editor.txt.text();
                 content.length > 60 ? content.slice( 0, 60 ) : content;
 
                 let str = `
-                        <div class="memo_item">
+                        <div class="memo_item" data=`+ res.id +` data-id="hide" data-type="memo">
                             <div class="item_title">
                                 <h5>` + $( ".memo_title" ).val() + `</h5>
                             </div>
@@ -199,9 +236,19 @@ function memoModlueFile(){
                     `;
                 $( ".memo_a" ).after( str );
 
-                loger( res );
+                //加了一行辣鸡代码 用来添加到首页html
+                $( ".home_items" ).prepend( str );
+                //欸这里会影响性能！
+                if( $( ".home_items>div[data-type='hide']" ).length >= 1 ) {
+                    $( ".home_items>div[data-type='hide']" ).eq(0).remove();
+                }
+
+                timeEvent();
+                loger( res[ "errmsg" ] );
                 $( ".memo_title" ).val( "" );
-                editor.txt.html( "" );
+                editor.txt.text( "" );
+                $(".audio_click").prop( "checked", false );
+                $("#datetime-picker").val("");
                 GoHashUrl( "memo" );
 
             } )
--- js/index-note.js
+++ js/index-note.js
@@ -48,69 +48,102 @@ function noteModuleFile () {
     }
 
     //长按事件
-    var timeOutEvent = null;
-    $( ".note_item" ).on( {
+    function timeEvent () {
 
-        touchstart: function ( e ) {
-            // 长按事件触发
-            let self = this;
-            timeOutEvent = setTimeout( function () {
-                timeOutEvent = 0;
+        var timeOutEvent = null;
+        $( ".note_item" ).on( {
 
-                if( $( self ).attr( "data") ) {
+            touchstart: function ( e ) {
+                // 长按事件触发
+                let self = this;
+                timeOutEvent = setTimeout( function () {
+                    timeOutEvent = 0;
+                    let nodeId = $( self ).attr( "data");
+                    if( nodeId ) {
 
-                    $.confirm( {
-                        title   : '提示',
-                        text    : '确定删除？',
-                        onOK    : function () {
-                            //点击确认
-                            noteModule.delteNoteInfoById( $( self ).attr( "data"), function ( err, res ) {
-                                if( err ) {
-                                    loger( err );
-                                    return;
-                                }
-                                $( self ).remove();
-
-                                if( $( ".note_item" ).length < 6 ) {
-                                    let len = $( ".note_item" ).length;
-
-                                    for ( let i = 0; i < 6 - len; i++ ) {
-                                        $( ".note_items" ).append( "<div class='note_item' style='opacity: 0'></div>" );
+                        $.confirm( {
+                            title   : '提示',
+                            text    : '确定删除？',
+                            onOK    : function () {
+                                //点击确认
+                                noteModule.delteNoteInfoById( nodeId, function ( err, res ) {
+                                    if( err ) {
+                                        loger( err );
+                                        return;
                                     }
-                                }
-                            } )
-                        }
-                    } );
+                                    $( self ).remove();
+
+                                    if( $( ".note_item" ).length < 6 ) {
+                                        let len = $( ".note_item" ).length;
+
+                                        for ( let i = 0; i < 6 - len; i++ ) {
+                                            $( ".note_items" ).append( "<div class='note_item' style='opacity: 0'></div>" );
+                                        }
+                                    }
+
+                                    //删除后没有让首页盒子的消失
+                                    //写两部分，第一如果存在id和type的话，就删除 如果不存在 是不是新添加的
+                                    //$( self ).attr( "data") 是当前id
+
+                                    if( $( ".home_items>div[id="+nodeId+"][data-type='note']" ) ) {
+
+                                        try {
+                                            $( ".home_items>div[id="+nodeId+"][data-type='note']" ).remove();
+                                        }catch ( e ) {
+                                            console.log( "remove home_item fail" );
+                                            console.log( e );
+                                        }
+
+                                    }else if( $( ".home_items>div[data-id='hide'][data-type='note']" ) ){
+                                        //如果是新增加的 是没有id的，
+                                        try {
+                                            $( ".home_items>div[data-id='hide'][data-type='note']" ).remove();
+                                        }catch ( e ) {
+                                            console.log( "remove home_item data-id=hide fail" );
+                                            console.log( e );
+                                        }
+                                    }
+
+
+                                    if( $( ".home_item" ).length < 7 ) {
+                                        let len = $( ".home_item" ).length;
+                                        for ( let i = 0; i < 7 - len; i++ ) {
+                                            $( ".home_items" ).append( "<div class='home_item' data-type='hide' style='opacity: 0'></div>" );
+                                        }
+                                    }
+
+
+                                } )
+                            }
+                        } );
+                    }
+                }, 1000 );
+                //长按400毫秒
+                e.preventDefault();
+            },
+            touchmove : function () {
+                clearTimeout( timeOutEvent );
+                timeOutEvent = 0;
+            },
+            touchend  : function () {
+                let self = this;
+                clearTimeout( timeOutEvent );
+                if ( timeOutEvent != 0 ) {
+                    // 点击事件
+                    //显示到记事本
+                    if( $(self).attr("data") || $(self).attr("data-id") ){
+                        $(".note_title").val( $( this ).children("div").eq(0).children("h5").eq(0).html() );
+                        editor1.txt.text( $( this ).children("div").eq(1).children("p").eq(0).html() );
+                        GoHashUrl( "note_input" );
+                    }
 
                 }
-
-
-            }, 1000 );
-            //长按400毫秒
-            e.preventDefault();
-        },
-        touchmove : function () {
-            clearTimeout( timeOutEvent );
-            timeOutEvent = 0;
-        },
-        touchend  : function () {
-            let self = this;
-            clearTimeout( timeOutEvent );
-            if ( timeOutEvent != 0 ) {
-                // 点击事件
-                //显示到记事本
-                if( $(self).attr("data") ) {
-
-                    $(".memo_title").val( $( this ).children("div").eq(0).children("h5").eq(0).html() );
-                    editor.txt.html( $( this ).children("div").eq(1).children("p").eq(0).html() );
-                    GoHashUrl( "memo_input" );
-                }
-
+                return false;
             }
-            return false;
-        }
-    } )
+        } )
 
+    }
+    timeEvent();
 
 
     //点击保存
@@ -122,7 +155,7 @@ function noteModuleFile () {
             return;
         }
 
-        if( !editor1.txt.html() ) {
+        if( !editor1.txt.text() ) {
             loger( "请填写内容" );
             return;
         }
@@ -130,7 +163,7 @@ function noteModuleFile () {
 
         let changeData = {
             "title"    : $(".note_title").val(),
-            "content"  : editor1.txt.html(),
+            "content"  : editor1.txt.text(),
 
         };
         noteModule.postNoteInfo( changeData, function (err, res) {
@@ -140,11 +173,11 @@ function noteModuleFile () {
             }
 
             //将最新的数据写入到html上
-            let content = editor1.txt.html();
+            let content = editor1.txt.text();
             content.length > 60?content.slice( 0, 60 ): content;
 
             let str = `
-                   <div class="memo_item">
+                   <div class="note_item" data=`+ res.id +` data-id="hide" data-type="note">
                         <div class="item_title">
                             <h5>`+ $(".note_title").val() +`</h5>
                         </div>
@@ -156,7 +189,20 @@ function noteModuleFile () {
                    </div>
                 `;
             $(".note_add").after( str );
-            loger( res );
+            //加了几行辣鸡代码 用来添加到首页html
+            if( $( ".home_items>div[data-type='note']" ).length >= 1 ) {
+                $( ".home_items>div[data-type='note']" ).eq(0).before( str );
+            }else{
+                $( ".home_items>div[data-type='hide']" ).eq(0).before( str );
+            }
+            timeEvent();
+
+            //得删除掉一个隐藏的div
+            if( $( ".home_items>div[data-type='hide']" ).length >= 1 ) {
+                $( ".home_items>div[data-type='hide']" ).eq(0).remove();
+            }
+
+            loger( res["errmsg"] );
             GoHashUrl( "notes" );
 
         })
